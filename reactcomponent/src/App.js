da1c8a470149714ce061bd8e654e3c40c7d2e28b
allassaniabdoulaziz@gmail.com <allassaniabdoulaziz@gmail.com>
2021-10-14 04:57:06 +0000

    upgrade

--- reactcomponent/src/App.js
+++ reactcomponent/src/App.js
@@ -1,26 +1,17 @@
-import './App.css';
-import Address from './Component/Profile/Address';
-import FullName from './Component/Profile/FullName';
-import ProfilePhoto from './Component/Profile/ProfilePhoto';
+import "./App.css";
+import Address from "./Component/Profile/Address";
+import FullName from "./Component/Profile/FullName";
+import ProfilePhoto from "./Component/Profile/ProfilePhoto";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">   
+      <header className="App-header">
         <ProfilePhoto />
         <p>
-        <FullName />
+          <FullName />
+          <Address />
         </p>
-        <p>
-         <Address />
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
       </header>
     </div>
   );
